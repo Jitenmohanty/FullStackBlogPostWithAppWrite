fa7918694857d44291a1c41c02a97427ddf68a17
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-25 11:54:14 +0530

    Appwrite setup Done

---
+++ BlogPostAppWrite/src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import { Client, Account, ID } from "appwrite";
+import config from "../config/config";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount() {
+    const userAccount = await this.account.create(
+      ID.unique(),
+      email,
+      password,
+      name
+    );
+
+    try {
+      if (userAccount) {
+        // Direct call the login method If user was created.
+        return this.login({ email, password });
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log("Appwrite serive :: Login :: error", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
