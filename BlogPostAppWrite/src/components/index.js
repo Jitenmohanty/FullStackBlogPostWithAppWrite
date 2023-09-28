b227354ff45de343fd4df9b76917f03617976695
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-28 19:37:40 +0530

    Some Components are added

--- BlogPostAppWrite/src/components/index.js
+++ BlogPostAppWrite/src/components/index.js
@@ -1,4 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Button from "./Button";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import Input from "./Input";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer, Logo, Button, Input, LogoutBtn, Container };
